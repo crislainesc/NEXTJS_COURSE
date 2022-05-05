cd3a35182e7137b4fd18005c453cc5ad5a88fc81
Crislâine Santos <criis.santos@outlook.com>
2022-05-05 11:07:48 -0300

    fix: files removed

--- src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Backdrop({ onCancel }) {
-	return <div className='backdrop' onClick={onCancel} />;
-}
-
-export default Backdrop;
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Backdrop from './Backdrop';
-import Modal from './Modal';
-import Todo from './Todo';
-
-export { Backdrop, Modal, Todo };
