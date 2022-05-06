237dc91e5ad5ac3361fc9d710b7b2fad400d4a55
Crislâine Santos <criis.santos@outlook.com>
2022-05-06 15:58:33 -0300

    feat: optimizations in the next events app added

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name='description' content='Find a lot of great events that a low you to evolve...'></meta>
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    },
+    revalidate: 1800
+  }
+}
+
+export default HomePage;
