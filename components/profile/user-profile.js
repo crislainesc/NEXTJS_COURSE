f33154518fddcbc24229531579731dbaee191ece
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-05-09 14:50:44 -0300

    feat: next auth app added

---
+++ components/profile/user-profile.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { getSession } from "next-auth/client";
+
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+
+function UserProfile() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(passwordData),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm onChangePassword={changePasswordHandler} />
+    </section>
+  );
+}
+
+export default UserProfile;
